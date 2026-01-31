0300e22fa81631bd4918d08b74bbe40816105e76
molmezses <mustafaolmezses@gmail.com>
2026-01-31 11:58:02 +0300

    Initial Commit

---
+++ TmdbRestApiCase/TmdbRestApiCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TmdbRestApiCaseApp.swift
+//  TmdbRestApiCase
+//
+//  Created by mustafaolmezses on 31.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TmdbRestApiCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
